5b0ad3118f7a86a8710d778e4447c53439680677
VyberMisellus <VyberMisellus@users.noreply.github.com>
2018-07-08 18:09:03 -0400

    W2V
    Added word2vec implementation, making an embedding matrix for each word in the sentences passed in

--- Mood2Vec.py
+++ Mood2Vec.py
@@ -12,6 +12,9 @@ Created on Sun Jul  8 11:20:58 2018
 import nltk
 import os
 import re
+import gensim.models as w2v
+import multiprocessing
+import codecs
 
 
 #Getting the 'trash' character packages
@@ -22,15 +25,14 @@ nltk.download("stopwords")
 subreddits = ['angry',"SuicideWatch",'depression','happy','BPD','mentalillness','sad','hate']
 #The path that the files were saved to
 path = r"C:\Users\Isaac Csekey\Documents\MoodData"
-all_subs = dict()
-print(type(all_subs))
 
+#Declaring an initial string for the sentences
+all_subs = u""
 #The function to read a list of files in each directory
 def readPost(parent_directory, subreddit):
     
     #What will be returned: A big dictionary!
-    all_subs = dict()
-    print(type(all_subs))
+    all_subs = u""
     
     for sub in subreddit:
         
@@ -39,46 +41,49 @@ def readPost(parent_directory, subreddit):
         #Navigating to the directory
         os.chdir(sub_path)
     
-        #Making an empty list of all the posts in the one subreddit
-        all_posts = []
-    
         #Taking a look at all the files in the subreddit directory and reading them in
         for root, dirs, files in os.walk("."):  
             for filename in files:
-                tempfile = open(str(filename),"r")
-                tempfile = str(tempfile.read())
-            
-                tempfile = tempfile.split("</(COMMENT)\>")
-                all_posts.append(tempfile)
-                
-        all_subs[sub] = all_posts
-        print(type(all_subs))
-    return all_subs
-
-#The function to convert all the posts and comments in the dictionaries to a list of lists of strings
-def toBigStrings(dictionary_of_subs):
-    
-    #Initialize the list variable
-    biglist = []
-    
-    #Chunking through each category in the dictionary
-    for entry in dictionary_of_subs:
-        category = dictionary_of_subs[entry]
+                with codecs.open(filename, 'r', 'utf-8') as postfile:
+                    try:
+                        all_subs += postfile.read()
+                    except UnicodeDecodeError:
+                        pass
         
-        #Chunking through each list in the category list of lists
-        for entries in category:
-            #Chunking through the list of entries lists
-            for ent in entries:
-                #Some 'preprocessing' to get only the words
-                clean = re.sub("[^a-zA-Z]"," ", ent)
-                #appending the list of strings given the cleaned data
-                biglist.append(clean)
-    return biglist
+                
+                #all_posts.append(tempfile)
+                
+    #Returning the lowercase words 
+    return all_subs.lower()
 
-print(type(all_subs))
+#The function to tokenize and clean the sentences, returning a list of lists that can be fed into word2vec
+def listOfWords(string):
+    
+    
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    sent_raw = tokenizer.tokenize(string)
+    
+    #The sentences list
+    s = []
+    for sentence in sent_raw:
+        if len(sentence)>0:
+            clean = re.sub("[^a-zA-Z]"," ", sentence)
+            s.append(clean.split())
+    return s
+    
+    
 
 #First getting the dictionary of raw data
 all_subs = readPost(path,subreddits)
 
-#Cleaning up and assigning a big list of words
-bigfile = toBigStrings(all_subs)
+#Cleaning the data
+sentences = listOfWords(all_subs)
+
+#Creating the word2vec
+sadvectors = w2v.Word2Vec(sentences, seed= 42, workers=multiprocessing.cpu_count(), size = 300, min_count = 3, window = 10, sample = 1e-3 )
+
+#Training the word2vec neural net
+sadvectors.train(sentences,total_examples=len(sentences),epochs=50)
+
+#Saving the model
+sadvectors.save('mood2vec.w2v')
