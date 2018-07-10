f4e86287b102b7aecb0aecaa1165ae72bd85e278
VyberMisellus <VyberMisellus@users.noreply.github.com>
2018-07-10 14:27:55 -0400

    Loading and Cleaning Data
    Created a function to load all reddit posts and create a dictionary of lists of each post, where the dictionary will then be used to create a feature and label set.
    
    The next step will be creating two lists: one of the features and one of the labels using the dictionary.

--- MoodLSTM.py
+++ MoodLSTM.py
@@ -10,8 +10,55 @@ import tensorflow as tf
 import tflearn as tl
 import gensim.models as w2v
 import os
-import pandas
+import re
 
 os.chdir(r"C:\Users\Isaac Csekey\Documents\MoodData")
 svctrs = w2v.KeyedVectors.load("mood2vec.w2v")
 
+#Setting up training and testing data 
+
+labelslist = subreddits = ['angry',"SuicideWatch",'depression','happy','BPD','mentalillness','sad','hate','mentalhealth','depression_help','depressionregimens','Anxiety']
+path = r"C:\Users\Isaac Csekey\Documents\MoodData"
+
+
+#A function to get the data from the directories and clean it up a bit
+def getData(subreddit_list):
+
+    #Dictionary instance for ease of labelling
+    labelled_data = dict()
+    
+    
+    #Chunking through all the items in the subreddit list to open up the post files, clean them up, and add them to the 
+    for item in subreddit_list:
+        os.chdir(os.path.join(path,item))
+        
+        #An empty list to store all the posts in
+        
+        category_list = []
+        
+        
+        #Going through all the files in the directory
+        for root, dirs, files in os.walk("."):
+            
+            #Going through all the files MORE
+            for filename in files:
+                
+                #An instance of a string that will act as a collector for all the text thrown into it
+                collectionstring = str()
+                file = open(filename, 'r')
+                for line in file:
+                    collectionstring += line
+                
+                print("Opened ",filename)
+                
+                
+                collectionstring = re.sub("[^a-zA-Z]"," ", collectionstring)
+                
+                category_list.append(collectionstring.lower())
+                
+        labelled_data[item] = category_list
+        print(item, "loaded to dictionary\n")
+        
+    return labelled_data
+
+data_dict = getData(labelslist)
